4893e4e4b1e92dc612e76af0f1bff9384c4045c9
Gary Anderson <ganderson@seattleacademy.org>
2015-04-29 08:37:17 -0700

    add sample.js

---
+++ sample.js
@@ -0,0 +1,56 @@
+var https = require('https');
+var fs = require('fs');
+var quotes = [];
+quotes.push("first quote");
+quotes.push("second quote");
+quotes.push("third quote");
+quotes.push("fourth quote");
+quotes.push("fifth quote");
+
+function getRandomQuote() {
+    return quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+var options = {
+    key: fs.readFileSync('/etc/ssl/server.key'),
+    cert: fs.readFileSync('/etc/ssl/server.crt')
+};
+
+function respond(theText) {
+
+    theResponse = {
+        version: '1.0',
+        response: {
+            outputSpeech: {
+                type: 'PlainText',
+                text: theText
+            },
+            card: {
+                type: 'Simple',
+                title: 'Sample',
+                subtitle: 'Spring 23',
+                content: theText
+            },
+            shouldEndSession: 'true'
+        }
+    }
+    return (theResponse);
+}
+
+
+https.createServer(options, function(req, res) {
+    if (req.method == 'POST') {
+        var jsonString = '';
+        req.on('data', function(data) {
+            jsonString += data;
+        });
+        req.on('end', function() {
+            console.log(JSON.parse(jsonString));
+        });
+    }
+    myResponse = JSON.stringify(respond(getRandomQuote()));
+    res.setHeader('Content-Length', myResponse.length);
+    res.writeHead(200);
+    res.end(myResponse);
+    console.log(myResponse);
+}).listen(443); //Put number in the 3000 range for testing and 443 for production
